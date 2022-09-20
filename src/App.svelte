08f50858f1f57c2167c4f7c035f58a738e26f7f2
Woon Eusean <woon.eusean@touchngo.com.my>
2022-09-20 20:10:37 +0800

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { Header, SkipToContent, Content, SideNav, SideNavItems, SideNavLink } from 'carbon-components-svelte';
+    import ToEntityManager from './lib/ToEntityManager.svelte';
+    import ToPojo from './lib/ToPojo.svelte';
+
+    let isSideNavOpen = false;
+    let currentPage = 0;
+</script>
+
+<Header company="JPA" platformName="Utilities" bind:isSideNavOpen>
+    <svelte:fragment slot="skip-to-content">
+        <SkipToContent />
+    </svelte:fragment>
+</Header>
+
+<SideNav bind:isOpen={isSideNavOpen}>
+    <SideNavItems>
+        <SideNavLink text="DDL to JPA POJO" on:click={() => (currentPage = 0)} />
+        <SideNavLink text="DDL to EntityManager Insert" on:click={() => (currentPage = 1)} />
+    </SideNavItems>
+</SideNav>
+
+<Content>
+    {#if currentPage == 0}
+        <ToPojo />
+    {:else}
+        <ToEntityManager />
+    {/if}
+</Content>
---
+++ src/lib/ToEntityManager.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { Grid, Row, Column } from 'carbon-components-svelte';
+</script>
+
+<Grid>
+    <Row>
+        <Column>
+            <h1>DDL to EntityManager Insert</h1>
+        </Column>
+    </Row>
+</Grid>
